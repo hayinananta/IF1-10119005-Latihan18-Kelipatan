c324c2f2b0b55fa576975be62806412e72da6729
Hayin Ananta <72958200+hayinananta@users.noreply.github.com>
2020-10-28 12:20:37 +0700

    Add files via upload

---
+++ src/if1/pkg10119005/latihan18/kelipatan/IF110119005Latihan18Kelipatan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119005.latihan18.kelipatan;
+
+/**
+ *
+ * @author Hayin
+ * NAMA     : Hayin Ananta
+ * KELAS    : IF-1
+ * NIM      : 10119005
+ * Deskripsi Program : Kelipatan
+ */
+public class IF110119005Latihan18Kelipatan {
+    static double kelipatan = 3.5;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i = 0; i < 9; i++){
+            kelipatan += 3.5;
+            System.out.println(kelipatan);
+        }
+    }
+    
+}
